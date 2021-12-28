65119af459c4904160c59d076fe04acdde9bbfc0
Jesus Zuñiga <jesuszt@hotmail.com>
2021-12-27 20:47:57 -0600

    Modificaciones 27/12/2021

--- src/main/java/com/zutjmx/springboot/app/gateway/filters/EjemploGlobalFilter.java
+++ src/main/java/com/zutjmx/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.MediaType;
+//import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -39,9 +39,9 @@ public class EjemploGlobalFilter implements GlobalFilter, Ordered {
 					.getCookies()
 					.add("color", ResponseCookie.from("color", "rojo").build());
 			
-			exchange.getResponse()
-					.getHeaders()
-					.setContentType(MediaType.TEXT_PLAIN);
+			/*
+			 * exchange.getResponse() .getHeaders() .setContentType(MediaType.TEXT_PLAIN);
+			 */
 			
 		}));
 	}
